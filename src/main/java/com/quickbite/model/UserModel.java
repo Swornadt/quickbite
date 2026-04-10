da1a3d08f870d994891b42cacacc1d40d0168880
Sanskar Piya <sanspiya915@gmail.com>
2026-04-10 22:58:48 +0545

    Add: DAO and MODEL file for hashing process added

---
+++ src/main/java/com/quickbite/model/UserModel.java
@@ -0,0 +1,70 @@
+package com.quickbite.model;
+
+public class UserModel {
+	
+	//Registration form input fields;
+	private String fname;
+	private String lname;
+	private String number;
+	private String email;
+	private String gender;
+	private String dob;
+	private String password;
+	
+	//Getter and Setter 
+	public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
