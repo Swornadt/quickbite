3b0cba35f9550df5b6efe27c478aa31a9d11649a
Swornadt <tuladharsworna@gmail.com>
2026-04-13 16:00:38 +0545

    feat: added controller for logout

---
+++ src/main/java/com/quickbite/controller/LogoutController.java
@@ -0,0 +1,29 @@
+package com.quickbite.controller;
+
+import jakarta.servlet.ServletException;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import com.quickbite.utils.SessionUtil;
+
+/**
+ * Servlet implementation class LogoutController
+ */
+@WebServlet(asyncSupported = true, urlPatterns = { "/LogoutController" })
+public class LogoutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public LogoutController() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		SessionUtil.invalidateSession(request);
+		response.sendRedirect(request.getContextPath()+"/login");
+	}
+
+}
