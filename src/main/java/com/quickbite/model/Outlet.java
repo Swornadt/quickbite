0ddcf3c44784d1b20417899549fb0343b1ca02ee
Resha <resha.koju.77@gmail.com>
2026-04-18 14:46:02 +0545

    feat: Added the Model and DAO for Outlet.jsp

---
+++ src/main/java/com/quickbite/model/Outlet.java
@@ -0,0 +1,32 @@
+package com.quickbite.model;
+
+public class Outlet {
+	private int outlet_id;
+	private String outlet_name;
+	private String outlet_status;
+	private String outlet_image;
+
+	public Outlet(int outlet_id, String outlet_name, String outlet_status, String outlet_image) {
+		this.outlet_id=outlet_id;
+		this.outlet_name=outlet_name;
+		this.outlet_status=outlet_status;
+		this.outlet_image=outlet_image;
+	}
+	
+	public int getOutletId() {
+		return outlet_id;
+	}
+	
+	public String getOutletName() {
+		return outlet_name;
+	}
+	
+	public String getOutletStatus() {
+		return outlet_status;
+	}
+	
+	public String getOutletImage() {
+		return outlet_image;
+	}
+	
+}
