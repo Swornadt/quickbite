577e90ada7bba49cdee7f7fc36218d2ab93659b7
Swornadt <tuladharsworna@gmail.com>
2026-04-27 06:58:07 +0545

    feat: implemented suburl routing to about

---
+++ src/main/java/com/quickbite/controller/AboutController.java
@@ -0,0 +1,62 @@
+package com.quickbite.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+import com.quickbite.model.CartItemModel;
+
+@WebServlet(asyncSupported = true, urlPatterns = { "/about/*" })
+public class AboutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public AboutController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String endpoint = request.getPathInfo();
+		
+		// routing on the basis of which endpoint is hit
+		switch (endpoint) {
+			case "/hridishna":
+		        request.getRequestDispatcher("/WEB-INF/views/public/about/hridishna.jsp").forward(request, response);
+		        break;
+		        
+		    case "/resha":
+		        request.getRequestDispatcher("/WEB-INF/views/public/about/resha.jsp").forward(request, response);
+		        break;
+		        
+		    case "/sabrina":
+		        request.getRequestDispatcher("/WEB-INF/views/public/about/sabrina.jsp").forward(request, response);
+		        break;
+		        
+		    case "/sahil":
+		        request.getRequestDispatcher("/WEB-INF/views/public/about/sahil.jsp").forward(request, response);
+		        break;
+		        
+		    case "/sanskar":
+		        request.getRequestDispatcher("/WEB-INF/views/public/about/sanskar.jsp").forward(request, response);
+		        break;
+		        
+		    case "/sworna":
+		        request.getRequestDispatcher("/WEB-INF/views/public/about/sworna.jsp").forward(request, response);
+		        break;
+		
+			default:
+				response.sendError(HttpServletResponse.SC_NOT_FOUND);
+				break;
+		}
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
