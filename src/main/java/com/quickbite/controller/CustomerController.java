eb22e96a152abd8527c6b3f7a7b920b6be6604b3
Swornadt <tuladharsworna@gmail.com>
2026-05-05 21:26:24 +0545

    refactor: creating routes for user profiles

---
+++ src/main/java/com/quickbite/controller/CustomerController.java
@@ -0,0 +1,68 @@
+package com.quickbite.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.quickbite.dao.OrderDAO;
+import com.quickbite.model.OrderModel;
+import com.quickbite.model.UserModel;
+
+@WebServlet(asyncSupported = true, urlPatterns = { "/customers/profile" })
+public class CustomerController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private OrderDAO orderDAO = new OrderDAO();
+	
+    public CustomerController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String endpoint = request.getServletPath();
+		
+		switch(endpoint) {
+			case "/":
+				request.getRequestDispatcher("/WEB-INF/views/customer/profile/user-profile.jsp").forward(request, response);
+				break;
+			case "/order-history":
+				viewCustomerOrderHistory(request, response);
+			default:
+				response.sendError(HttpServletResponse.SC_NOT_FOUND);
+				break;
+		}
+	}
+
+	private void viewCustomerOrderHistory(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		HttpSession session = request.getSession();
+		UserModel user = (UserModel) session.getAttribute("user");
+		if (user == null) {
+			response.sendRedirect(request.getContextPath() + "/login");
+			return;
+		}
+		
+		// fetch data by dao
+		List<OrderModel> currentOrders = orderDAO.getOrdersByStatus(user.getUserId(), true);
+        List<OrderModel> pastOrders = orderDAO.getOrdersByStatus(user.getUserId(), false);
+        
+        // setting those attributes to jsp
+        request.setAttribute("currentOrders", currentOrders);
+        request.setAttribute("pastOrders", pastOrders);
+        
+		request.getRequestDispatcher("/WEB-INF/views/customer/profile/order-history.jsp").forward(request, response);		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
