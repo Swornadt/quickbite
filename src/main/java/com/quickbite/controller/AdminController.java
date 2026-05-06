578d4a434b5383458cd426dc64a7d21ab1d80f1e
Resha <resha.koju.77@gmail.com>
2026-05-06 10:55:14 +0545

    Feat: Added the Functionality for Admin to update the Gender from their profile, and also added the navigation of settings to the Admin Profile Page

--- src/main/java/com/quickbite/controller/AdminController.java
+++ src/main/java/com/quickbite/controller/AdminController.java
@@ -320,12 +320,13 @@ public class AdminController extends HttpServlet {
 					int user_id = Integer.parseInt(request.getParameter("user_id"));
 					String fname = request.getParameter("fname");
 					String lname = request.getParameter("lname");
+					String gender = request.getParameter("gender");
 					String email = request.getParameter("email");
 					String number = request.getParameter("number");
 							
 					//Update via service
 					AdminService adminService = new AdminService();
-					boolean success = adminService.updateAdminProfile(user_id, fname, lname, email, number);
+					boolean success = adminService.updateAdminProfile(user_id, fname, lname, gender, email, number);
 					
 					if(success) {
 						response.sendRedirect(request.getContextPath() + "/admin/profile?update=success");
