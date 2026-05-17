d99eccf568722b2e62e7c88152c79dc6d4a2d171
Swornadt <tuladharsworna@gmail.com>
2026-05-17 07:57:30 +0545

    feat: added strict rbac for auth, user roles

--- src/main/java/filter/AuthFilter.java
+++ src/main/java/filter/AuthFilter.java
@@ -37,39 +37,67 @@ public class AuthFilter extends HttpFilter {
 		
         HttpSession session = req.getSession(false);
         String path = req.getServletPath();
-        UserModel user = (session == null) ? null : (UserModel) session.getAttribute("user");
-
         
+        UserModel user = (session == null) ? null : (UserModel) session.getAttribute("user");
+        String role = (user != null) ? user.getRole() : null;
+        
+        // Logged-in users shouldn't revisit auth pages
+        if (path.equals("/login") || path.equals("/register")) {
+        	if (user != null) {
+        		
+        		if ("admin".equalsIgnoreCase(role)) {
+                	res.sendRedirect(req.getContextPath() + "/admin");
+                	
+                } else if ("staff".equalsIgnoreCase(role)) {
+                	res.sendRedirect(req.getContextPath() + "/kitchen");
+                	
+                } else if ("customer".equalsIgnoreCase(role)) {
+                	res.sendRedirect(req.getContextPath() + "/home");
+                }
+        		return;
+        	}
+        }
         
         // Admin Access
         if (path.startsWith("/admin")) {
-            if (user == null || !"admin".equalsIgnoreCase(user.getRole())) {
-                res.sendRedirect(req.getContextPath() + "/home");
-                return;
-            }
-        }
-        
-        // Customer Access
-        if (path.startsWith("/checkout") || path.startsWith("/profile")) {
             if (user == null) {
                 res.sendRedirect(req.getContextPath() + "/login");
                 return;
             }
-        }
-        
-        // Logged-in users shouldn't revisit auth pages
-        if (path.equals("/login") || path.equals("/register")) {
-            if (user != null) {
-                res.sendRedirect(req.getContextPath() + "/home");
-                return;
+            if (!"admin".equalsIgnoreCase(role)) {
+            	res.sendError(HttpServletResponse.SC_FORBIDDEN, "Unauthorized: Admin access required.");
+            	return;
             }
         }
         
+        // Staff Access
+        else if (path.startsWith("/kitchen")) {
+            if (user == null) {
+                res.sendRedirect(req.getContextPath() + "/login");
+                return;
+            }
+            if (!"staff".equalsIgnoreCase(role)) {
+            	res.sendError(HttpServletResponse.SC_FORBIDDEN, "Unauthorized: Staff access required.");
+            	return;
+            }
+        }
+        
+        // Customer Access
+        else if (path.startsWith("/checkout") || path.startsWith("/profile") || path.startsWith("/cart") || path.startsWith("/profile")) {
+            if (user == null) {
+                res.sendRedirect(req.getContextPath() + "/login");
+                return;
+            }
+            if (!"customer".equalsIgnoreCase(role)) {
+            	res.sendError(HttpServletResponse.SC_FORBIDDEN, "Unauthorized: Customer access required.");
+            	return;
+            }
+        }
+        
+        // for public path or permissions align we proceed
         chain.doFilter(req, res);
     }
 		
-	public void init(FilterConfig fConfig) throws ServletException {
-		// Initialization logic if needed
-		
+	public void init(FilterConfig fConfig) throws ServletException {	
 	}
 }
