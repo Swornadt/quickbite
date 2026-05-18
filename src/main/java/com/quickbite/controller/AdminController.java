427969933bc0a655aff82b117756b29dbbc72fcc
Swornadt <tuladharsworna@gmail.com>
2026-05-18 06:17:30 +0545

    chore: removed debugging print

--- src/main/java/com/quickbite/controller/AdminController.java
+++ src/main/java/com/quickbite/controller/AdminController.java
@@ -106,7 +106,6 @@ public class AdminController extends HttpServlet {
 				viewMenuAdd(request, response);
 				break;
 			case "/menu/edit":
-				System.out.println("edit");
 				viewMenuEdit(request, response);
 				break;
 			case "/menu/delete":
