816625153b72b2b5eacec9a781beaca56bce485e
Swornadt <tuladharsworna@gmail.com>
2026-05-18 16:59:44 +0545

    chore: added DBconfig and ImageUtil javadocs

--- src/main/java/com/quickbite/utils/DBconfig.java
+++ src/main/java/com/quickbite/utils/DBconfig.java
@@ -12,6 +12,14 @@ public class DBconfig {
 	private static final String USER = "root";
 	private static final String PASSWORD = "";
 	
+	/**
+	 * Starts and returns a live connection to the database.
+	 * 
+	 * This method registers the MySQL JDBC driver class and attempts to
+	 * initialize a connection using the predefined URL, username and password.
+	 * 
+	 * @return active Connection instance if the database connection is successful; null otherwise.
+	 */
 	public static Connection getConnection() {
 		Connection conn = null;
 		try {
